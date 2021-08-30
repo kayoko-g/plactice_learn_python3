d3a7d2d053888c70367cc215061078e501ea88b3
kayoko-g <k.tahara03@gmail.com>
2021-08-31 06:33:14 +0900

    エクササイズ5　続き

--- ex5.py
+++ ex5.py
@@ -16,3 +16,64 @@ print(f'彼の歯はたいてい{my_teeth}だが、それはコーヒー次第�
 #この行は不思議に思えるかもしれないが、とにかく試してみよう。
 total = my_age + my_height + my_weight
 print(f'{my_age}と{my_height}と{my_weight}を足すと{total}だ。')
+
+
+#1)
+name = 'Zed A. Shaw'
+age = 39 #嘘じゃないよ
+height = 74 #インチ
+weight = 180 #ポンド
+eyes = '青色'
+hair = '茶色'
+teeth = '白色'
+
+print(f'{name}について語ろう。')
+print(f'彼の身長は{height}インチ。')
+print(f'彼の体重は{weight}ポンド。')
+print('実際のところ、そんなに太ってはいない。')
+print(f'彼の目の色は{eyes}で、髪の色は{hair}だ。')
+print(f'彼の歯はたいてい{teeth}だが、それはコーヒー次第だ。')
+
+#この行は不思議に思えるかもしれないが、とにかく試してみよう。
+total = age + height + weight
+print(f'{age}と{height}と{weight}を足すと{total}だ。')
+
+#2
+name = 'Zed A. Shaw'
+age = 39 #嘘じゃないよ
+height = round(74 * 2.54) #センチメートル
+weight = round(180 * 0.45) #キログラム
+eyes = '青色'
+hair = '茶色'
+teeth = '白色'
+
+print(f'{name}について語ろう。')
+print(f'彼の身長は{height}センチ。')
+print(f'彼の体重は{weight}キロ。')
+print('実際のところ、そんなに太ってはいない。')
+print(f'彼の目の色は{eyes}で、髪の色は{hair}だ。')
+print(f'彼の歯はたいてい{teeth}だが、それはコーヒー次第だ。')
+
+#この行は不思議に思えるかもしれないが、とにかく試してみよう。
+total = age + height + weight
+print(f'{age}と{height}と{weight}を足すと{total}だ。')
+
+#3
+name = '森'
+age = 30 #嘘じゃないよ
+height = 158 #センチメートル
+weight = 45 #キログラム
+eyes = '黒色'
+hair = '茶色'
+teeth = '白色'
+
+print(f'{name}について語ろう。')
+print(f'彼の身長は{height}センチ。')
+print(f'彼の体重は{weight}キロ。')
+print('実際のところ、そんなに太ってはいない。')
+print(f'彼の目の色は{eyes}で、髪の色は{hair}だ。')
+print(f'彼の歯はたいてい{teeth}だが、それはコーヒー次第だ。')
+
+#この行は不思議に思えるかもしれないが、とにかく試してみよう。
+total = age + height + weight
+print(f'{age}と{height}と{weight}を足すと{total}だ。')
