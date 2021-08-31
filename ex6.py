88a1743e024595a3e3e22950e0b79c9d04469ea3
kayoko-g <k.tahara03@gmail.com>
2021-09-01 07:20:38 +0900

    エクササイズ6　続き

--- ex6.py
+++ ex6.py
@@ -1,20 +1,32 @@
+#変数types_of_peopleに10を代入
 types_of_people = 10
+#xにf'世の中には{types_of_people}種類の人間がいる。'を代入
 x = f'世の中には{types_of_people}種類の人間がいる。'
 
+#binaryに'バイナリ'を代入
 binary = 'バイナリ'
+#do_notに'そうでない'を代入
 do_not = 'そうでない'
+#yにf'{binary}を知っている人と、{do_not}人だ。'を代入
 y = f'{binary}を知っている人と、{do_not}人だ。'
 
+#printにて、上記変数xとyを表示
 print(x)
 print(y)
 
+#printにてf'私はいった:{x}'を表示
 print(f'私はいった:{x}')
+#printにてf'こうともいった:"{y}"'を表示
 print(f'こうともいった:"{y}"')
 
+#hilariousにFalseを代入
 hilarious = False
+#joke_evaluationに'このジョークは面白かったかな?!{}'を代入
 joke_evaluation = 'このジョークは面白かったかな?!{}'
 
+#joke_evaluation.format(hilarious)をprintにて表示
 print(joke_evaluation.format(hilarious))
+
 
 w = 'これは左側のテキストで…'
 e = 'これは右側のテキストだ。'
