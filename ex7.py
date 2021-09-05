2e4310bb687b96ac89018a9555e8c3f93cd5e398
kayoko-g <k.tahara03@gmail.com>
2021-09-06 05:30:35 +0900

    エクササイズ7続き

--- ex7.py
+++ ex7.py
@@ -1,22 +1,38 @@
-
+#printにてMariy had a little lamb を表示させる。 
 print('Mary had a little lamb.')
+#printにて　Its fleece was white as snow を表示させる。
 print('Its fleece was white as {}.'.format('snow'))
+#printにて　And everywhere that Maruy went を表示させる。
 print('And everywhere that Mary went.')
+#printにて'.'*10 を表示させる。この表示は.が10個表示される。
 print('.'*10) #これは何をするだろうか？
-
+#end1 に　Cを代入
 end1 = 'C'
+#end2 にhを代入
 end2 = 'h'
+#end3　にeを代入
 end3 = 'e'
+#end4 にeを代入
 end4 = 'e'
+#end5 にsを代入
 end5 = 's'
+#end6 にを代入
 end6 = 'e'
+#end7にBを代入
 end7 = 'B'
+#end8にuを代入
 end8 = 'u'
+#end9にrを代入
 end9 = 'r'
+#end10にgを代入
 end10 = 'g'
+#end11にeを代入
 end11 = 'e'
+#end12にrを代入
 end12 = 'r'
 
 #最後のend=''に注目しよう。これを取り除くとどうなるだろうか？
-print(end1 + end2 + end3 + end4 + end5 + end6, end='')
+#さっきまで作っていた変数をprint内で表示
+print(end1 + end2 + end3 + end4 + end5 + end6, end='　')
+#上記童謡に変数をprint 内で表示
 print(end7 + end8 + end9 + end10 + end11 + end12)
