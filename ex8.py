578e1724df34aac94668abf96f8c2b0b1b9627a0
kayoko-g <k.tahara03@gmail.com>
2021-09-06 06:38:52 +0900

    エクササイズ8途中

---
+++ ex8.py
@@ -0,0 +1,17 @@
+#formatter に{} {} {} {}を代入する
+formatter = "{} {} {} {}"
+#printにて、
+print(formatter.format(1,2,3,4))
+#printにて 
+print(formatter.format('一','二','三','四'))
+#printにて
+print(formatter.format(True,False,False,True))
+#printにて
+print(formatter.format(formatter,formatter,formatter,formatter))
+#printにて
+print(formatter.format(    
+    '好きな文を',
+    'ここに書いてみよう',
+    'たとえば詩や',
+    '歌なんかもよいだろう'
+))
