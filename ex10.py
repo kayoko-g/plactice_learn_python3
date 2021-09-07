65ea9bca5f3a3f926f2d86b013e60735ac49d5d5
kayoko-g <k.tahara03@gmail.com>
2021-09-08 05:41:49 +0900

    エクササイズ10　続き1

--- ex10.py
+++ ex10.py
@@ -1,6 +1,11 @@
+#変数　tabby_cat　に　\tこれはタブ。を代入。\t　はタブを表している
 tabby_cat = '\tこれはタブ。'
+#変数 persian_cat に これは行を\n分割。を代入。　\n は改行を表している。
 persian_cat = 'これは行を\n分割。'
+#変数 backslash_cat に これは\\一匹の\\猫。を代入。\\のうち一つが表示される。
 backslash_cat = 'これは\\一匹の\\猫。'
+
+#変数fat_cat に """'猫'用のリスト：\t* キャットフード\t* 魚\t* マタタビ\n\t* 猫草"""を代入している。"""により改行を、\tによりスペースを　\nにより改行を行っている。
 
 fat_cat = """
 '猫'用のリスト：
@@ -8,7 +13,7 @@ fat_cat = """
 \t* 魚
 \t* マタタビ\n\t* 猫草
 """
-
+#プリントにより上で作った変数を表示させている。
 print(tabby_cat)
 print(persian_cat)
 print(backslash_cat)
