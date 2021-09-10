3cc0d8bb2b5623b836fba30d4a8519a19a4a9950
kayoko-g <k.tahara03@gmail.com>
2021-09-11 03:31:51 +0900

    エクササイズ13　続き

--- ex13.py
+++ ex13.py
@@ -3,19 +3,19 @@ from sys import argv
 
 #このコードを実行する方法は「実行結果」を参照すること。
 #引数をargvに代入する。
-script, first, second, third = argv
+#script, first, second, third = argv
 
 #printにて'このスクリプトの名前は:'を表示させ、続けて引数のscript を表示させる。
-print('このスクリプトの名前は:', script)
+#print('このスクリプトの名前は:', script)
 #printにて'first変数の値は:'を表示させ、続て引数のfirst を表示させる。
-print('first変数の値は:', first)
+#print('first変数の値は:', first)
 #printにて'second変数の値は:'を表示させ、続けて引数のsecond を表示させる。
-print('second変数の値は:', second)
+#print('second変数の値は:', second)
 #printにて、'third変数の値は:'を表示させ、続けて引数のthird　を表示させる。
-print('third変数の値は:', third)
+#print('third変数の値は:', third)
 
 #引数をargvに代入する。
-script, apple, orange, banana,grape = argv
+script, apple, orange, banana, grape = argv
 
 #printにて'このスクリプトの名前は:'を表示させ、続けて引数のscript を表示させる。
 print('このスクリプトの名前は:', script)
@@ -24,4 +24,9 @@ print('appeleの個数は:', apple)
 #printにて'second変数の値は:'を表示させ、続けて引数のsecond を表示させる。
 print('orangeの個数は:', orange)
 #printにて、'third変数の値は:'を表示させ、続けて引数のthird　を表示させる。
+print('grapeの個数は:', banana)
+#printにて、'third変数の値は:'を表示させ、続けて引数のthird　を表示させる。
 print('grapeの個数は:', grape)
+
+eat = input('どれが食べたい?')
+print(f'君は{eat}が食べたいのだね。')
