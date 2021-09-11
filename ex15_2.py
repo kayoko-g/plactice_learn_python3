2f714cef68633bae7955d35357a9a2b167b93067
kayoko-g <k.tahara03@gmail.com>
2021-09-12 07:11:51 +0900

    エクササイズ15　closeを追加

--- ex15_2.py
+++ ex15_2.py
@@ -1,6 +1,11 @@
 
- 
-script, filename = argv
+#inputのみで記述→コードが長くなる。実行するのも少々面倒。
+from os import close, terminal_size
+
+
+script = input('スクリプト名を入力してみよう:') 
+filename = input('読みたいファイル名を入力してみよう:')
+
 #txt に open(filename,encoding='utf-8')　を代入する。 open()はファイルを返す組み込み関数
 txt = open(filename,encoding='utf-8')
 
@@ -10,6 +15,8 @@ print(f'{filename}の内容は次のとおり：')
 #〇.read()にて〇のファイルを読みだしている
 print(txt.read())
 
+txt.close()
+
 #printにて「もう一度ファイル名を入力しよう：」を表示
 print('もう一度ファイル名を入力しよう：')
 #file_againにinput('> ')を代入。>より後に入力された文字列がfile_againに代入される。
@@ -18,3 +25,5 @@ file_again = input('> ')
 txt_again = open(file_again,encoding='utf-8')
 #printにてtxt_again.read()を表示。ここでは、txt_againにて読みだしたファイルを表示させている。
 print(txt_again.read())
+
+txt_again.close
