9c6aa3c8755ed74a6fa35885ad095b8ca81c57ca
kayoko-g <k.tahara03@gmail.com>
2021-09-24 05:58:07 +0900

    エクササイズ19　演習3 3個書いてみた

--- ex19-1.py
+++ ex19-1.py
@@ -1,6 +1,19 @@
 def odogu(sumi_count,kami_count,fude_count):
-    print(f'墨が{sumi_count}本、紙が{kami_count}冊、筆が{fude_count}。')
-    print('価格は筆、墨、紙の順に高くなる。')
-
+    print(f'墨が{sumi_count}本、紙が{kami_count}冊、筆が{fude_count}本。')
+  
+#1個目
 odogu(1,1,2)
 
+
+#2個目
+print('お道具の在庫を増やした。')
+add_sumi = 10
+add_kami = 12
+add_fude = 20
+odogu(1 + add_sumi, 1 + add_kami, 2 + add_fude)
+
+#3個目
+print('購入されたので在庫が減った')
+odogu(11 - 8, 13 - 8, 22 - 8)
+
+#４個目
