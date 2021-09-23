6f3557a751b9286f09f01f0390c223fe3e75e6e5
kayoko-g <k.tahara03@gmail.com>
2021-09-24 06:56:15 +0900

    エクササイズ20　演習①

--- ex20.py
+++ ex20.py
@@ -1,18 +1,22 @@
+#argvをインポートする。
 from sys import argv
 
+#script, input_file をargvに代入する。
 script, input_file = argv
 
+#以降3つが関数。
+#①print_all関数は、引数ｆで受け取ったファイルを.read()部分で読みだしている。
 def print_all(f):
     print(f.read())
-
+#②rewind関数は、fで受け取ったファイルを
 def rewind(f):
     f.seek(0)
-
+#③
 def print_a_line(line_count, f):
     print(line_count, f.readline())
 
 
-current_file = open(input_file, encording = 'utf-8')
+current_file = open(input_file, encoding = 'utf-8')
 
 print('まずファイル全体を出力する:')
 
