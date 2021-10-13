392490f4373525610e802b3e54b43579b3dfe75c
kayoko-g <k.tahara03@gmail.com>
2021-10-14 06:39:49 +0900

    エクササイズ21　見直し

--- ex21.py
+++ ex21.py
@@ -1,16 +1,16 @@
 def add(a, b):
-    print(f'加算{a} + {b}')
+    print(f'加算 {a} + {b}')
     return a + b
 
-def subtract(a,b):
-    print(f'減算 {a} + {b}')
+def subtract(a, b):
+    print(f'減算 {a} - {b}')
     return a - b
 
 def multiply(a, b):
     print(f'乗算 {a} * {b}')
-    return a + b
+    return a * b
 
-def devide(a, b):
+def divide(a, b):
     print(f'除算 {a} / {b}')
     return a / b
 
@@ -20,13 +20,13 @@ print('関数を使って計算をやってみよう!')
 age = add(30, 9)
 height = subtract(78, 4)
 weight = multiply(90, 2)
-iq = devide(100, 2)
+iq = divide(100, 2)
 
-print(f'年齢:{age},身長：{height},体重:{weight}, IQ: {iq}')
+print(f'年齢: {age}, 身長: {height}, 体重: {weight}, IQ: {iq}')
 
-#追加のパズル。とにかくこれを入力しよう。
+# 追加のパズル。とにかくこれを入力しよう。
 print('パズルをやってみよう。')
 
-what = add(age, subtract(height),multiply(weight, devide(iq, 2)))
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
 
-print('結果：',what,'手で計算したのと同じ結果になっただろうか?')
+print('結果:', what,'手で計算したのと同じ結果になっただろうか?')
